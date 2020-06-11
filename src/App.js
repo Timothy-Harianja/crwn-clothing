4320da9ddcaf88e231a98b761e68304d0c64cc94
Timothy-Harianja <timothy.harianja@gmail.com>
2020-06-11 14:24:53 -0400

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ v
         <Header/>
         <Switch>
           <Route exact path='/' component={HomePage}/>
-          <Route exact path='/shop' component={ShopPage}/>
+          <Route path='/shop' component={ShopPage}/>
           <Route exact path='/checkout' component={CheckoutPage}/>
           <Route 
             exact
