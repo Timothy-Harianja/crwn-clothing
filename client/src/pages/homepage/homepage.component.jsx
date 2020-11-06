7b29d66c6d547e97d1c5ff7b935044e80357f9e4
Timothy-Harianja <timothy.harianja@gmail.com>
2020-11-05 22:57:26 -0500

    adding performance optimization to codebase with react lazy and memo

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -4,10 +4,14 @@ import Directory from '../../components/directory/directory.component';
 
 import { HomePageContainer } from './homepage.styles';
 
-const HomePage = (props) => (
-    <HomePageContainer>
-        <Directory />
-    </HomePageContainer>
-);
+const HomePage = () => {
+    return (
+        <HomePageContainer>
+            <Directory />
+        </HomePageContainer>
+    )
+
+}
+
 
 export default HomePage;
