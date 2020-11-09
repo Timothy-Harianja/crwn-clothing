167903eca185308c934fedf078a0edd5c9aabad4
Timothy-Harianja <timothy.harianja@gmail.com>
2020-11-09 10:53:21 -0500

    PWA

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 if(process.env.NODE_ENV == 'production') {
+    app.use(compression());
     //serve service worker for PWA
     app.get('/service-worker.js', (req, res) => {
         res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
